f54798fd12f531a6352d6c295e1b32bed168a7e0
shahriar hossain <shahriar_cse@live.com>
2018-06-22 13:04:43 +0600

    refactoring functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
 //Create the component/jsx
-const App = ()=>{
-return (<div>
+class App extends Component {
+    render(){
+        return (<div>
             <h1>What Are You Looking For? </h1> 
             <div><SearchBar /> </div>
         </div>);
+    } 
 }
 
 //Render the component generated HTML and put it inside the DOM
