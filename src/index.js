5dd362007eef3eb0eda31f3b0a3f424c4056c338
shahriar hossain <shahriar_cse@live.com>
2018-06-22 14:37:50 +0600

    fetching youtube videos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+const YOUTUBE_API_KEY = 'AIzaSyAtvq0lOlbyjjLy4jXmxBElMpPQkSCC2XY';
+
 
 //Create the component/jsx
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videoList : [] 
+        }
+
+        YTSearch({ key: YOUTUBE_API_KEY, term : 'selena'}, videos =>{
+            this.setState({
+                videoList: videos
+            });
+        });
+    }
+
     render(){
         return (<div>
             <h1>What Are You Looking For? </h1> 
