40a725eaa37d76c888b0cd689bedfba38579381c
shahriar hossain <shahriar_cse@live.com>
2018-06-22 10:46:02 +0600

    creating functional component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 //Create the component/jsx
 const App = ()=>{
-    return <div><h1>Hello from React </h1></div>;
+return (<div>
+            <h1>What You Are Looking For? </h1> 
+            <div><SearchBar /> </div>
+        </div>);
 }
 
 //Render the component generated HTML and put it inside the DOM
